4ac2620b04d33a74807824a57c48ad7bb48654ca
unknown <Michael Lau>
2020-05-01 21:21:03 -0400

    ch

--- Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-namespace Todo.Api.Models
-{
-    public class TodoItem
-    {
-        public int Id { get; set; }
-        public string Action { get; set; }
-        public bool IsDone { get; set; }
-    }
-}
